5d391f892368ccd3fec896d27cf7f2a150d986a0
VinayDagar <vinay.dagar102@gmail.com>
2022-01-23 02:51:05 +0530

    added feed containers

--- src/pages/Feeds/index.js
+++ src/pages/Feeds/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, FlatList } from 'react-native';
 
 const Feed = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const data = Array.apply(null, Array(62)).map((v, i) => {
+      return {
+        id: i,
+        src: 'http://placehold.it/200x200?text=' + (i + 1),
+        isReel: false,
+      };
+    });
+    setItems(data);
+  }, []);
+
+  const feedItemGenerator = ({ item }) => (
+    <View style={styles.feed}>
+      <Image style={styles.imageThumbnail} source={{ uri: item.src }} />
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>This is Feed Screen</Text>
+      <FlatList
+        data={items}
+        numColumns={3}
+        keyExtractor={(item, index) => index}
+        renderItem={feedItemGenerator}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
@@ -16,5 +41,14 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    flex: 1,
+  },
+  feed: {
+    margin: 1,
+  },
+  imageThumbnail: {
+    height: 120,
+    width: 120,
   },
 });
